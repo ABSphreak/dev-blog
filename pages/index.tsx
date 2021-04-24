9b7f93028deb92405439f62e4e059a5e63fa4e99
ABSphreak <absphreak@outlook.com>
2021-04-24 20:15:39 +0530

    ➕ Add Loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { Loader } from '../components/Loader';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h1>Something Awesome</h1>;
+  return (
+    <div>
+      <Loader show />
+    </div>
+  );
 }
