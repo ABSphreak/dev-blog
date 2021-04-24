591ea026f5d141f96af2215dbae5a7b697c92797
ABSphreak <absphreak@outlook.com>
2021-04-25 02:19:08 +0530

    ➕ Added more code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import Link from 'next/link';
 
-const Navbar = () => {
-  return <div>This is navbar</div>;
+export const Navbar = () => {
+  const user = null;
+  const username = null;
+
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} alt="Profile avatar" />
+              </Link>
+            </li>
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 };
-
-export default Navbar;
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,3 +1,41 @@
+import { auth, googleAuthProvider } from '../lib/firebase';
+
 export default function EnterPage() {
-  return <main>Sign up</main>;
+  const user = null;
+  const username = null;
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+}
+
+// Sign in with Google Button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={`/google.png`} alt="Google Sign in" /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out Button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign out</button>;
+}
+
+// Form
+function UsernameForm() {
+  return <></>;
 }
